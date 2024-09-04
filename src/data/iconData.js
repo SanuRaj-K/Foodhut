e3dd1b2d3bd7a397a63a08f77ddbfc38c3aef56d
SanuRaj-K <sanusrk29@gmail.com>
2024-09-04 11:02:02 +0530

    first commit

---
+++ src/data/iconData.js
@@ -0,0 +1,79 @@
+import Airbnb from "../assets/icons/Airbnb-Logo.svg";
+import Amazon from "../assets/icons/Amazon.svg";
+import AMD from "../assets/icons/AMD.svg";
+import Apple from "../assets/icons/Apple-Pay.svg";
+import Arista from "../assets/icons/Arista-Logo.svg";
+import Bitcoin from "../assets/icons/Bitcoin.svg";
+import Copyright from "../assets/icons/Copyright.svg";
+import Dropbox from "../assets/icons/Dropbox-Logo.svg";
+import FedEx from "../assets/icons/FedEx-Logo.svg";
+import GPay from "../assets/icons/Google-pay.svg";
+import LeftArrow from "../assets/icons/Icons=Arrow-left.svg";
+import RightArrow from "../assets/icons/Icons=Arrow-Right.svg";
+import Bevrages from "../assets/icons/Icons=Beverages.svg";
+import Cart from "../assets/icons/Icons=Cart.svg";
+import Contact from "../assets/icons/Icons=Contact.svg";
+import Dryfruits from "../assets/icons/Icons=Dry-fruits.svg";
+import Visa from "../assets/icons/Visa.svg";
+import Stripe from "../assets/icons/Stripe.svg";
+import Spotify from "../assets/icons/Spotify.svg";
+import Paypal from "../assets/icons/Paypal.svg";
+import Mastercard from "../assets/icons/Master-Card.svg";
+import Vegetalbles from "../assets/icons/Icons=Vegetables.svg";
+import Twitter from "../assets/icons/Icons=Twitter.svg";
+import Tea from "../assets/icons/Icons=Tea.svg";
+import Support from "../assets/icons/Icons=Support.svg";
+import Snack from "../assets/icons/Icons=Snack.svg";
+import SecurePayment from "../assets/icons/Icons=Secure Payment.svg";
+import Quotes from "../assets/icons/Icons=Quotes.svg";
+import Pinterest from "../assets/icons/Icons=Pinterest.svg";
+import Milk from "../assets/icons/Icons=Milk.svg";
+import Mail from "../assets/icons/Icons=Mail.svg";
+import Location from "../assets/icons/Icons=Location.svg";
+import Locaction1 from "../assets/icons/Icons=Location-1.svg";
+import Insta from "../assets/icons/Icons=Insta.svg";
+import Fruites from "../assets/icons/Icons=Fruits.svg";
+import Freeshipping from "../assets/icons/Icons=Free-Shipping.svg";
+import Facebook from "../assets/icons/Icons=Facebook.svg";
+import EasyReturn from "../assets/icons/Icons=Easy-Return.svg";
+
+export {
+  EasyReturn,
+  Snack,
+  Amazon,
+  Apple,
+  Arista,
+  Bitcoin,
+  Copyright,
+  Dropbox,
+  FedEx,
+  GPay,
+  LeftArrow,
+  RightArrow,
+  Bevrages,
+  AMD,
+  Tea,
+  Support,
+  Twitter,
+  Vegetalbles,
+  Mastercard,
+  Paypal,
+  Spotify,
+  Stripe,
+  Visa,
+  Dryfruits,
+  Contact,
+  Cart,
+  Airbnb,
+  Facebook,
+  Freeshipping,
+  Fruites,
+  Insta,
+  Locaction1,
+  Location,
+  Mail,
+  Milk,
+  Pinterest,
+  Quotes,
+  SecurePayment,
+};
---
+++ src/data/imageData.js
@@ -0,0 +1,45 @@
+import Smoothie from "../assets/images/All-Smoothie.jpg";
+import Banana from "../assets/images/Banana.png";
+import Carrot from "../assets/images/Carrot.png";
+import Cupcake from "../assets/images/Cupcake.png";
+import Dates from "../assets/images/Dates.png";
+import EnergyDrink from "../assets/images/Energy-Drink.png";
+import EverDay from "../assets/images/Everyday-Fresh.jpg";
+import FreeShippingImg from "../assets/images/Free-Shipping.jpg";
+import GreenMusk from "../assets/images/Green-Muskmelon.png";
+import GreenPear from "../assets/images/Green-Pear.png";
+import Kiwi from "../assets/images/Kiwi.png";
+import LandingBG from "../assets/images/Landing-Background.jpg";
+import Landing1 from "../assets/images/Landing-Background.jpg";
+import Landing2 from "../assets/images/Landing-img-2.jpg";
+import MilkIMG from "../assets/images/Milk.png";
+import Olive from "../assets/images/Olive.png";
+import Pasta from "../assets/images/Pasta.png";
+import Pineapple from "../assets/images/Pineapple.png";
+import Sale from "../assets/images/Sale-img-kiwi.jpg";
+import Tomato from "../assets/images/Tomato.png";
+import Organic from "../assets/images/Organic-Product.jpg";
+
+export {
+  Organic,
+  Smoothie,
+  Banana,
+  Carrot,
+  Cupcake,
+  Dates,
+  Tomato,
+  Sale,
+  Pineapple,
+  Pasta,
+  Olive,
+  MilkIMG,
+  Landing1,
+  Landing2,
+  LandingBG,
+  Kiwi,
+  GreenMusk,
+  GreenPear,
+  FreeShippingImg,
+  EverDay,
+  EnergyDrink,
+};
